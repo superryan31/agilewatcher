54384b1ca7f12cbcd01764cac537cd07158ab081
Nan Khin Mhwe <nankhinmhwe56@gmail.com>
2019-08-20 16:57:50 +0630

    Remove not using function in navigation drawer

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -28,7 +28,10 @@
                     <v-list-item-action>
                         <v-icon>exit_to_app</v-icon>
                     </v-list-item-action>
-                    Logout
+                    <v-list-item-content>
+                        <v-list-item-title>Logout</v-list-item-title>
+                    </v-list-item-content>
+
                 </v-list-item>
 
 
@@ -60,35 +63,13 @@
             </v-btn>
             <v-toolbar-title v-text="title"/>
             <v-spacer/>
-            <v-btn
-                    icon
-                    @click.stop="rightDrawer = !rightDrawer"
-            >
-                <v-icon>mdi-menu</v-icon>
-            </v-btn>
         </v-app-bar>
         <v-content>
             <v-container>
                 <nuxt/>
             </v-container>
         </v-content>
-        <v-navigation-drawer
-                v-model="rightDrawer"
-                :right="right"
-                temporary
-                fixed
-        >
-            <v-list>
-                <v-list-item @click.native="right = !right">
-                    <v-list-item-action>
-                        <v-icon light>
-                            mdi-repeat
-                        </v-icon>
-                    </v-list-item-action>
-                    <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+
         <v-footer
                 :fixed="fixed"
                 app
@@ -102,8 +83,8 @@
   export default {
     data() {
       return {
-        clipped: false,
-        drawer: false,
+        clipped: true,
+        drawer: true,
         fixed: false,
         items: [
           {
@@ -123,8 +104,6 @@
           }
         ],
         miniVariant: false,
-        right: true,
-        rightDrawer: false,
         title: 'Agile Watcher'
       }
     },
